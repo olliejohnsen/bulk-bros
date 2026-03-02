989ad7c56b5ca3cb391dc65dcf0a73a4a2ba2ecd
ollie <ollie@cookquest.app>
2026-03-02 03:12:12 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
-    localPatterns: [
-      {
-        pathname: "/uploads/**",
-        search: "",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
